b6725bc87241133b1c36f23bdfc23a016b2ac65b
ereyes0212 <erick.reyes@tiempo.hn>
2025-05-15 10:22:15 -0600

    cambio puerto

--- app/generated/prisma/index.js
+++ app/generated/prisma/index.js
@@ -384,7 +384,7 @@ const config = {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "mysql://alquiler_user:X7@cZ$91uN!eRt3q@18.189.20.117:3306/alquiler"
+        "value": "mysql://alquiler_user:X7%40cZ%2491uN%21eRt3q@18.189.20.117:3306/alquiler\n"
       }
     }
   },
