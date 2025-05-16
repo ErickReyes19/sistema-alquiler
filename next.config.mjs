84e3ba34fbe69ce78bebc7dead47b25b3c9bd610
ereyes0212 <erick.reyes@tiempo.hn>
2025-05-16 10:37:11 -0600

    cambio PRISMA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// next.config.js
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(new PrismaPlugin());
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
